0b0cd1bd11e5955ef1ef153ae1d160ef63e42998
geoff winner & Conor Baumgart <student@epicodus.com>
2015-03-04 10:04:06 -0800

    accepts tasks in form

--- src/task.php
+++ src/task.php
@@ -5,16 +5,26 @@
 
         function __construct($description)
     {
-        $this->description = $description;
+            $this->description = $description;
     }
         function setDescription($new_description)
     {
-        $this->description = (string) $new_description;
+            $this->description = (string) $new_description;
     }
 
         function getDescription()
     {
-        return $this->description;
+            return $this->description;
+    }
+
+        function save()
+    {
+            array_push($_SESSION['list_of_tasks'], $this);
+    }
+
+        static function getALL()
+    {
+            return $_SESSION['list_of_tasks'];
     }
 }
 ?>
